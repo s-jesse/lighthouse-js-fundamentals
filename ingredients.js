6864c112e4ba758f1c1102f1b622d20a5f8b07c7
Jesse Sandhu <s.jesse23@gmail.com>
2023-04-09 04:07:33 -0700

    different loops for an array recipe

---
+++ ingredients.js
@@ -0,0 +1,17 @@
+const ingredients = ["eggs", "milk", "flour", "sugar", "baking soda", "baking powder", "chocolate chips", "bananas"];
+
+// Write a while loop that prints out the contents of ingredients:
+let i = 0;
+while (i < ingredients.length) {
+  console.log(ingredients[i]);
+  i++;
+}
+
+// Write a for loop that prints out the contents of ingredients:
+for (let x = 0; x < ingredients.length; x++) {
+  console.log(ingredients[x]);
+}
+// Write any loop (while or for) that prints out the contents of ingredients backwards:
+for (let n = ingredients.length - 1; n >= 0; n--) {
+  console.log(ingredients[n]);
+}
